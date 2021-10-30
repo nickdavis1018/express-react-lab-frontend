44e4a1a9729854e81ad788a171b4ce6198bfdcfe
Nicholas Davis <nickdavis1018@gmail.com>
2021-10-30 12:30:29 -0700

    cleaned up code/comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
-
-// IMPORT COMPONENTS
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Route, Switch } from "react-router-dom";
-// IMPORT PAGES
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 
 function App() {
-  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
   const URL = "https://express-react-lab-nd.herokuapp.com/";
 
   return (
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import router
 import { BrowserRouter as Router } from "react-router-dom";
 
-// wrap our application inside of router to enable using router
 ReactDOM.render(
   <Router>
     <React.StrictMode>
@@ -16,7 +14,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
